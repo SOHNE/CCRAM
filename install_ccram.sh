000a25274db3df189a3412528795b513774db4df
Sohne <leandrogonzagaperes@hotmail.com>
2016-08-04 15:01:48 -0300

    install_ccram.sh atualizado
    Fora adicionado algumas correções que ao tentar executar o install_ccram 2x ou mais, impossibilitaria de copiar o ccram para o /usr/bin/

--- install_ccram.sh
+++ install_ccram.sh
@@ -1,16 +1,14 @@
 #!/bin/sh
 {
-    echo "Este script irá 'instalar' o ccram (Clean Cache RAM)."
-    echo "You will be prompted for your password by sudo."
-
-    sudo -k
-
-    sudo sh <<SCRIPT
-   cd /tmp
-   wget https://sohne.com.br/sh/ccram
-   sudo chmod +x ccram
-   sudo cp ccram /usr/bin
-
+echo "Este script irá copiar o CCRAM para pasta /tmp e depois para /usr/bin"
+echo "You will be prompted for your password by sudo."
+sudo -k
+sudo sh <<SCRIPT
+cd /tmp
+rm ccram* &> /dev/null
+wget https://sohne.com.br/sh/ccram
+sudo chmod +x ccram
+sudo cp ccram /usr/bin
 SCRIPT
 echo "O ccram foi instalado. Para usar, utilize o comando ccram. (provavelmente a senha sudo sera requisitada)"
 }
